894e0cbea993d6c2cb632cc7027fecbb50d99b30
6254 <6254@TV6254NB>
2017-05-06 17:31:03 +0800

    練習建立 ViewModel 並建立一個 /Products/ListProducts 頁面 (使用 ViewModel 快速建立頁面)

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 
 namespace MVC5Course.Controllers
 {
@@ -127,6 +128,20 @@ namespace MVC5Course.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult ListProducts()
+        {
+            var data = db.Product.Where(p => p.Active == true)
+                .Select(p => new ProductListVM()
+                {
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Price = p.Price,
+                    Stock = p.Stock
+                }).Take(10);
+
+            return View(data);
         }
     }
 }
---
+++ MVC5Course/Models/ViewModels/ProductListVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ProductListVM
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        public Nullable<decimal> Stock { get; set; }
+    }
+}
