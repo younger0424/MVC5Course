578c2295a94ebeaf3c1c75f1c43355402662acef
6254 <6254@TV6254NB>
2017-05-06 16:26:11 +0800

    練習在 ProductName 加上 [DisplayName] 自訂欄位標題名稱

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,8 +2,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
@@ -11,16 +12,20 @@ namespace MVC5Course.Models
     
     public partial class ProductMetaData
     {
-        [Required]
+
         public int ProductId { get; set; }
         [Required]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [DisplayName("商品名稱")]
         public string ProductName { get; set; }
         [Required]
         [DisplayFormat(DataFormatString ="{0:0}",ApplyFormatInEditMode = true)]
+        [DisplayName("商品價格")]
         public Nullable<decimal> Price { get; set; }
+        [DisplayName("是否上架")]
         public Nullable<bool> Active { get; set; }
         [Required]
+        [DisplayName("是否有庫存")]
         public Nullable<decimal> Stock { get; set; }
     
         public virtual ICollection<OrderLine> OrderLine { get; set; }
